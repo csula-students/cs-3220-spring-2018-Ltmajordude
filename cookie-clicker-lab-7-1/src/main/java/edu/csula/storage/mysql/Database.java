17b42d96c4420db084b9aa00ec70f2eb9d73eb53
Ltmajordude <alalexalex152@gmail.com>
2018-05-06 23:10:26 -0700

    Did some small edits. Not all requirements are met. Not confident with this lab.

--- cookie-clicker-lab-7-1/src/main/java/edu/csula/storage/mysql/Database.java
+++ cookie-clicker-lab-7-1/src/main/java/edu/csula/storage/mysql/Database.java
@@ -7,10 +7,10 @@ import java.sql.SQLException;
 public class Database implements edu.csula.storage.Database {
 	// FIXME: don't hard commit your credential
 	// Or else the whole internet knows your username/password
-	private static final String database = "local_database"; //cs3220_lab
+	private static final String database = "cs3220stu46"; //cs3220_lab
 	private static final String host = "cs3.calstatela.edu"; //localhost
 	private static final String url = String.format("jdbc:mysql://%s/%s", host, database);
-	private static final String username = "cs3220stu46"; //root
+	private static final String username = "root"; //root
 	private static final String password = ""; //[blank]
 
 	public Database() {
